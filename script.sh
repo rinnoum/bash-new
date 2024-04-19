48b761e4f3ea134600cb9a53d51cba5d65607972
rinnoum <167458363+rinnoum@users.noreply.github.com>
2024-04-19 21:36:47 +0700

    Update script.sh
    Update All

--- script.sh
+++ script.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 
-# Ini adalah skrip shell untuk menjalankan skrip Python
+# URL tempat file Python disimpan di GitHub
+python_script_url="https://raw.githubusercontent.com/rinnoum/bash-new/main/simple_script.py"
+
+# Nama file Python setelah diunduh
+python_script_file="simple_script.py"
 
 # Menampilkan pesan sambutan
 echo "Menjalankan skrip Python..."
 
-# Menjalankan skrip Python
-python https://github.com/rinnoum/bash-new/raw/main/simple_script.py
+# Mengunduh file Python dari GitHub
+wget -O $python_script_file $python_script_url
+
+# Memeriksa apakah pengunduhan berhasil
+if [ $? -eq 0 ]; then
+    echo "File Python berhasil diunduh."
+    # Menjalankan skrip Python
+    python $python_script_file
+    # Menghapus file Python setelah dijalankan
+    rm $python_script_file
+else
+    echo "Gagal mengunduh file Python."
+fi
 
 # Selesai
 echo "Skrip telah selesai dijalankan."
