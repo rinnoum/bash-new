90d4c1bb39be6c41e105821592e0e96409adfe5b
rinnoum <167458363+rinnoum@users.noreply.github.com>
2024-04-19 21:30:27 +0700

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Ini adalah skrip shell untuk menjalankan skrip Python
+
+# Menampilkan pesan sambutan
+echo "Menjalankan skrip Python..."
+
+# Menjalankan skrip Python
+python simple_script.py
+
+# Selesai
+echo "Skrip telah selesai dijalankan."
