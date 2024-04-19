57427ceca92871214525c76ba8bf5e370ebde0d0
rinnoum <167458363+rinnoum@users.noreply.github.com>
2024-04-19 21:39:29 +0700

    Update simple_script.py

--- simple_script.py
+++ simple_script.py
@@ -1,5 +1,28 @@
-# simple_script.py
+# deep_learning_training.py
 
-# Ini adalah skrip Python sederhana
-print("Halo dari skrip Python!")
-print("Ini adalah contoh skrip Python yang dijalankan oleh skrip shell.")
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Generate some dummy data
+x_train = np.random.random((1000, 20))
+y_train = np.random.randint(2, size=(1000, 1))
+x_test = np.random.random((100, 20))
+y_test = np.random.randint(2, size=(100, 1))
+
+# Define the model
+model = Sequential()
+model.add(Dense(64, input_dim=20, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile the model
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
+
+# Save the model
+#model.save('simple_model.h5')
+
+print("Pelatihan model selesai dan model disimpan sebagai 'simple_model.h5'.")
